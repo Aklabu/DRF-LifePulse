596b360d85d8253ec1506625a71e96b75a3935e8
Md. Joni Akter Labu <mdjoniakterlabu@gmail.com>
2026-05-24 03:29:21 +0600

    add serializer validations

---
+++ apps/monitoring/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import CheckIn, MonitoringLog
+
+
+class CheckInSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CheckIn
+        fields = ['note']
+
+
+class CheckInResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CheckIn
+        fields = ['checked_in_at', 'date', 'note']
+
+
+class MonitoringStatusSerializer(serializers.ModelSerializer):
+    checked_in_at = serializers.SerializerMethodField()
+
+    class Meta:
+        model = MonitoringLog
+        fields = ['date', 'scheduled_check_in_time', 'deadline', 'status', 'checked_in_at']
+
+    def get_checked_in_at(self, obj):
+        # Return the timestamp of the check-in if status is checked_in
+        if obj.status == MonitoringLog.STATUS_CHECKED_IN:
+            check_in = obj.user.check_ins.filter(date=obj.date).order_by('-checked_in_at').first()
+            if check_in:
+                return check_in.checked_in_at
+        return None
